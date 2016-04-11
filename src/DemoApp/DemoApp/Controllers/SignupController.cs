0fef173c22d2558c4ecc186639f748b0506d4b14
Javier Lozano <javier@lozanotek.com>
2016-04-11 17:00:56 -0500

    Initial commit for source

---
+++ src/DemoApp/DemoApp/Controllers/SignupController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class SignupController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(string email)
+        {
+            return RedirectToAction("index", "home");
+        }
+    }
+}
